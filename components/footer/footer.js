17cced30e38799c7bcd22f3ac67c5cd27ad05272
marie <dvorzak.marie@gmx.at>
2021-04-02 17:53:51 +0200

    implement new system for templates

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -4,11 +4,16 @@
 var script1 = document.createElement('script');
 script1.src = '/components/img/img.js';
 document.head.appendChild(script1);
+
+var h1WithBottomMargin = document.createElement('script');
+h1WithBottomMargin.src = '/components/h1-with-bottom-margin/h1-with-bottom-margin.js';
+document.head.appendChild(h1WithBottomMargin);
+
 // IMPORT IMG GALLERY 
 var scriptImgGallery = document.createElement('script');
 scriptImgGallery.src = '/components/img-gallery/img-gallery.js';
 document.head.appendChild(scriptImgGallery);
-// IMPORT IMG GALLERY 
+// IMPORT Script top menu
 var scriptTopMenu = document.createElement('script');
 scriptTopMenu.src = '/components/top_menu/top_menu.js';
 document.head.appendChild(scriptTopMenu);
@@ -33,11 +38,17 @@ const template = document.createElement('template');
 var styleLink = '<link rel="stylesheet" href="./components/footer/footer.css">';
 createTemplate(styleLink);
 
-
+if (currentTemplate) {
+  if (currentTemplate === "Visualization-Almendra-Display") {
+    var templateStyle = '<link rel="stylesheet" href="./css/Visualization-Almendra-Display.css">';
+    createTemplate(templateStyle);
+    console.log(templateStyle)
+  }
+}
 
 //WRITE THE HTML FOR EACH COMPONENT HERE
 function createTemplate(link) {
-  template.innerHTML = styleLink + `
+  template.innerHTML = styleLink + templateStyle + `
   <div id="footer">
   <div>
     <h1></h1>
@@ -55,7 +66,11 @@ class Footer extends HTMLElement {
     if (this.getAttribute('base') === "false") {
       console.log("false")
       styleLink = '<link rel="stylesheet" href="/../../components/footer/footer.css">';//../../components/footer/footer.css
+      if (currentTemplate === "Visualization-Almendra-Display") {
+        templateStyle = '<link rel="stylesheet" href="../../css/Visualization-Almendra-Display.css">';
+      }
       createTemplate(styleLink);
+      createTemplate(templateStyle);
     }
 
     this.shadowRoot.appendChild(template.content.cloneNode(true));
---
+++ components/h1-with-bottom-margin/h1-with-bottom-margin.js
@@ -0,0 +1,23 @@
+const H1WithBottomMarginTemplate = document.createElement('template');
+H1WithBottomMarginTemplate.innerHTML = templateStyle + `
+<style>
+
+</style>
+
+<h1>
+
+</h1>
+`;
+
+class H1WithBottomMargin extends HTMLElement {
+  constructor() {
+    super();
+
+    this.showInfo = true;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(H1WithBottomMarginTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector('h1').innerHTML = this.getAttribute('title');
+  }
+}
+
+window.customElements.define('h1-with-bottom-margin', H1WithBottomMargin);
