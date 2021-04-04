e81f61b5f7e7723d83467e9ee028b508426fe838
marie <dvorzak.marie@gmx.at>
2021-04-04 23:19:42 +0200

    updates

--- components/header-single/header-single.js
+++ components/header-single/header-single.js
@@ -10,23 +10,23 @@ HeaderTemplate.innerHTML = allStyles + `
 <div class="container-fluid d-flex justify-content-center header-bg">
 <span id="slot-h1"></span>
 <span id="slot-img"></span>
-
 </div> 
 `;
 
 class HeaderSingle extends HTMLElement {
+
+
   constructor() {
     super();
-
     this.showInfo = true;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(HeaderTemplate.content.cloneNode(true));
 
     this.shadowRoot.getElementById('slot-h1').innerHTML = "<h1-with-bottom-margin id='headline' title='" + this.getAttribute('title') + "' class='mt-5'></h1-with-bottom-margin>";
-    this.shadowRoot.getElementById('slot-img').innerHTML = "<img-full-width-var-height ImgPath='"+this.getAttribute("imgPath")+"' altImg='"+this.getAttribute("altImg")+"' Caption='This is an image'></img-full-width-var-height>"
-
+    this.shadowRoot.getElementById('slot-img').innerHTML = "<img-full-width-var-height ImgPath='" + this.getAttribute("imgPath") + "' altImg='" + this.getAttribute("altImg") + "' Caption='This is an image'></img-full-width-var-height>"
     this.shadowRoot.querySelector('img-full-width-var-height').imgPath = this.getAttribute("imgPath");
   }
 }
 
 window.customElements.define('header-single', HeaderSingle);
+
