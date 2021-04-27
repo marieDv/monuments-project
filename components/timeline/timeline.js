4aa005f7eaf260499377fbb821db46b464f9a641
marie <dvorzak.marie@gmx.at>
2021-04-27 12:46:29 +0200

    more messy timeline updates

--- components/timeline/timeline.js
+++ components/timeline/timeline.js
@@ -15,6 +15,10 @@ timelineTemplate.innerHTML = allStyles + `
 .vis-item {
   border: pink 5px solid !important;
 }
+.img-container {
+  border: 2px solid red;
+  width: 20px;
+}
 </style>
 
 
@@ -34,28 +38,38 @@ class Timeline extends HTMLElement {
 
 
 
-setTimeout(() => {
- 
-// DOM element where the Timeline will be attached
-var container = document.getElementById("render-timeline-here");
+    setTimeout(() => {
+      var item6 =
+        '<strong>Statue gets errected</strong><br><div class="img-container"><img src="./media/car.png"  style="width:48px; height:48px;"></div>';
 
-// Create a DataSet (allows two way data-binding)
-var items = new vis.DataSet([
-  { id: 1, content: "item 1\naöskdjaslökdjaklsödjöaskldj", start: "2014-04-20" },
-  { id: 2, content: "item 2", start: "2014-04-14" },
-  { id: 3, content: "item 3", start: "2014-04-18" },
-  { id: 4, content: "item 4", start: "2014-04-16", end: "2014-04-19" },
-  { id: 5, content: "item 5", start: "2014-04-25" },
-  { id: 6, content: "item 6", start: "2019-04-27", type: "point" },
-]);
+      var item5 = '<strong>This is the headline</strong><br> <p>This is a much longer text withouth an image</p><span>April 21.</span>'
+      // DOM element where the Timeline will be attached
+      var container = document.getElementById("render-timeline-here");
 
-// Configuration for the Timeline
-var options = {};
+      // Create a DataSet (allows two way data-binding)
+      var items = new vis.DataSet([
+        // { id: 1, content: "item 1\naöskdjaslökdjaklsödjöaskldj", start: "2014-04-20", end:  "2018-04-20"},
+        { id: 2, content: "item 2", start: "2015-04-14" },
+        { id: 3, content: "item 3", start: "2016-04-18" },
+        { id: 4, content: "item 4", start: "2019-04-16" },
+        { id: 5, content: item5, start: "2014-04-25" },
+        { id: 6, content: item6, start: "2022-04-27" },
+      ]);
 
-// Create a Timeline
-var timeline = new vis.Timeline(container, items, options);
+      // Configuration for the Timeline
+      var options = {
+        width: '100%',
+        height: '500px',
+        margin: {
+          item: 20
+        },
+        showCurrentTime: false,
+      };
 
-}, 1000);
+      // Create a Timeline
+      var timeline = new vis.Timeline(container, items, options);
+
+    }, 1000);
 
 
 
