fb2354cdb6f7d5ddaed5054c3aa0fbe68f7691d6
marie <dvorzak.marie@gmx.at>
2021-05-03 14:23:08 +0200

    updates folder structure

--- components/full/footer/footer.js
+++ components/full/footer/footer.js
@@ -18,35 +18,35 @@ document.head.appendChild(functionsFile);
 
 
 var h1WithBottomMargin = document.createElement('script');
-h1WithBottomMargin.src = '/components/h1-with-bottom-margin/h1-with-bottom-margin.js';
+h1WithBottomMargin.src = '/components/base/h1-with-bottom-margin/h1-with-bottom-margin.js';
 document.head.appendChild(h1WithBottomMargin);
 
 
 //IMPORT H2-WITH-BOTTOM-MARGIN
 var scriptH2WithBottomMargin = document.createElement('script');
-scriptH2WithBottomMargin.src = '/components/h2-with-bottom-margin/h2-with-bottom-margin.js';
+scriptH2WithBottomMargin.src = '/components/base/h2-with-bottom-margin/h2-with-bottom-margin.js';
 document.head.appendChild(scriptH2WithBottomMargin);
 
 //IMPORT H3-WITH-BOTTOM-MARGIN
 var scriptH3WithBottomMargin = document.createElement('script');
-scriptH3WithBottomMargin.src = '/components/h3-with-bottom-margin/h3-with-bottom-margin.js';
+scriptH3WithBottomMargin.src = '/components/base/h3-with-bottom-margin/h3-with-bottom-margin.js';
 document.head.appendChild(scriptH3WithBottomMargin);
 
 
 //PULL-QUOTE-WITH-BOTTOM-MARGIN-VAR-WIDTH
 var scriptPullQuoteWithBottomMarginVarWidth = document.createElement('script');
-scriptPullQuoteWithBottomMarginVarWidth.src = '/components/pull-quote-with-bottom-margin-var-width/pull-quote-with-bottom-margin-var-width.js';
+scriptPullQuoteWithBottomMarginVarWidth.src = '/components/base/pull-quote-with-bottom-margin-var-width/pull-quote-with-bottom-margin-var-width.js';
 document.head.appendChild(scriptPullQuoteWithBottomMarginVarWidth);
 
 //BUTTON
 var scriptButton = document.createElement('script');
-scriptButton.src = '/components/button/button.js';
+scriptButton.src = '/components/base/button/button.js';
 document.head.appendChild(scriptButton);
 
 
 // 
 var BlockQuoteWithLeftAndBottomMarginVarWidthScript = document.createElement('script');
-BlockQuoteWithLeftAndBottomMarginVarWidthScript.src = '/components/block-quote-with-left-and-bottom-margin-var-width/block-quote-with-left-and-bottom-margin-var-width.js';
+BlockQuoteWithLeftAndBottomMarginVarWidthScript.src = '/components/base/block-quote-with-left-and-bottom-margin-var-width/block-quote-with-left-and-bottom-margin-var-width.js';
 document.head.appendChild(BlockQuoteWithLeftAndBottomMarginVarWidthScript);
 
 
@@ -57,34 +57,34 @@ document.head.appendChild(BlockQuoteWithLeftAndBottomMarginVarWidthScript);
 // ******************************
 // VARIABLE TEXT
 var variableTextScript = document.createElement('script');
-variableTextScript.src = '/components/variable-text/variable-text.js';
+variableTextScript.src = '/components/base/variable-text/variable-text.js';
 document.head.appendChild(variableTextScript);
 
 
 // IMPORT image-full-width-var-height
 var scriptImageFullWidthVarHeight = document.createElement('script');
-scriptImageFullWidthVarHeight.src = '/components/image-full-width-var-height/image-full-width-var-height.js';
+scriptImageFullWidthVarHeight.src = '/components/base/image-full-width-var-height/image-full-width-var-height.js';
 document.head.appendChild(scriptImageFullWidthVarHeight);
 
 // IMPORT image-6-columns-var-height
 var scriptImgSixColumnsVarHeight = document.createElement('script');
-scriptImgSixColumnsVarHeight.src = '/components/image-six-columns-var-height/image-six-columns-var-height.js';
+scriptImgSixColumnsVarHeight.src = '/components/base/image-six-columns-var-height/image-six-columns-var-height.js';
 document.head.appendChild(scriptImgSixColumnsVarHeight);
 
 // IMPORT Script top menu
 var scriptTopMenu = document.createElement('script');
-scriptTopMenu.src = '/components/top_menu/top_menu.js';
+scriptTopMenu.src = '/components/full/top_menu/top_menu.js';
 document.head.appendChild(scriptTopMenu);
 
 // IMPORT Header 
 var scriptHeaderSingle = document.createElement('script');
-scriptHeaderSingle.src = '/components/header-single/header-single.js';
+scriptHeaderSingle.src = '/components/full/header-single/header-single.js';
 document.head.appendChild(scriptHeaderSingle);
 
 
 // IMPORT Timeline 
 var scriptTimeline = document.createElement('script');
-scriptTimeline.src = '/components/timeline/timeline.js';
+scriptTimeline.src = '/components/full/timeline/timeline.js';
 document.head.appendChild(scriptTimeline);
 
 // START IMPORT HALF WIDTH COMPONENTS
@@ -92,7 +92,7 @@ document.head.appendChild(scriptTimeline);
 
 // IMPORT TWO IMAGES EQUAL WIDTH
 var scriptTwoImagesSameWidthVarHeight = document.createElement('script');
-scriptTwoImagesSameWidthVarHeight.src = '/components/two-images-same-width-var-height/two-images-same-width-var-height.js';
+scriptTwoImagesSameWidthVarHeight.src = '/components/halves/two-images-same-width-var-height/two-images-same-width-var-height.js';
 document.head.appendChild(scriptTwoImagesSameWidthVarHeight);
 
 // IMPORT TWO IMAGES EQUAL WIDTH
@@ -111,7 +111,7 @@ const template = document.createElement('template');
 var styleBase = '<link rel="stylesheet" href="./css/baseStyle.css">';
 createTemplate(styleBase);
 
-var styleFooter = '<link rel="stylesheet" href="./components/footer/footer.css">';
+var styleFooter = '<link rel="stylesheet" href="./components/full/footer/footer.css">';
 createTemplate(styleFooter);
 
 var bootstrapLink = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" ></link>';
@@ -152,7 +152,7 @@ class Footer extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     if (this.getAttribute('base') === "false") {
       styleBase = '<link rel="stylesheet" href="./../../css/baseStyle.css">'
-      styleFooter = '<link rel="stylesheet" href="/../../components/footer/footer.css">';//../../components/footer/footer.css
+      styleFooter = '<link rel="stylesheet" href="/../../components/full/footer/footer.css">';//../../components/footer/footer.css
       if (currentTemplate === "Visualization-Almendra-Display") {
         templateStyle = '<link rel="stylesheet" href="../../css/Visualization-Almendra-Display.css">';
       }
