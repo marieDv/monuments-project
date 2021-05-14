24f68350b80f7c36228f87617c360497d3bcd29d
serr-o <serra.alexandre.1998@gmail.com>
2021-05-14 15:09:52 +0200

    text-4-images

--- components/full/footer/footer.js
+++ components/full/footer/footer.js
@@ -119,6 +119,7 @@ document.head.appendChild(scriptTwohalvesImageLeftTextRight);
 makeScript('/components/thirds/3-column-text-image-wide/3-column-text-image-wide.js');
 makeScript('/components/thirds/3-headlines-3-images/3-headlines-3-images.js');
 makeScript('/components/thirds/4-images-text/4-images-text.js');
+makeScript('/components/thirds/text-4-images/text-4-images.js');
 makeScript('/components/thirds/2-columns-text-images-right/2-columns-text-images-right.js');
 
 // ******************************************************************************************************************************************************
---
+++ components/thirds/text-4-images/text-4-images.js
@@ -0,0 +1,71 @@
+// IMAGE LEFT TEXT RIGHT
+// Attributes: altimg, imgPath, caption, variable text slots
+
+
+const ThirdsText4ImagesTemplate = document.createElement("template");
+ThirdsText4ImagesTemplate.innerHTML = allStyles + `
+
+<style>
+#container {
+  margin-top: 36px;
+}
+
+</style>
+
+<div id="container" class="container-fluid">
+<div class="row">
+
+<div class="col-md-4 col-sm-12">
+<span id="variable-text-slot"></span>
+</div>
+
+<div class="col-md-8 col-sm-12 container-fluid">
+<div class="row">
+
+  <div class="col-md-6 col-sm-12">
+    <span id="1-img"></span>
+    <span id="2-img"></span>
+  </div>
+  <div class="col-md-6 col-sm-12">
+   <span id="3-img"></span>
+   <span id="4-img"></span>
+  </div>
+
+</div>
+</div>
+
+
+</div>
+</div> 
+
+`;
+
+//initializing our class
+class ThirdsText4Images extends HTMLElement {
+  constructor() {
+    super();
+    setupComponent(this, ThirdsText4ImagesTemplate);
+
+    createComponentFromSlot(this, '1-img');
+    createComponentFromSlot(this, '2-img');
+    createComponentFromSlot(this, '3-img');
+    createComponentFromSlot(this, '4-img');
+    createVariableComponents(this, 'variable-text-slot');
+
+  }
+}
+window.customElements.define('thirds-text-4-images', ThirdsText4Images);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
